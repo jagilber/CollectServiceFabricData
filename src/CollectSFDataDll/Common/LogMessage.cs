af55cd94efca73d66491be3045039f9ad00eab83
Jason Gilbertson <jagilber@microsoft.com>
2021-05-09 00:07:23 -0400

    fix for error One or more errors occurred. (One or more errors occurred. (Unable to translate Unicode character \\uDF01 at index 409 to specified code page.))

--- src/CollectSFDataDll/Common/LogMessage.cs
+++ src/CollectSFDataDll/Common/LogMessage.cs
@@ -4,16 +4,34 @@
 // ------------------------------------------------------------
 
 using System;
+using System.Text;
 
 namespace CollectSFData.Common
 {
     public class LogMessage : EventArgs
     {
+        private string _message;
         public ConsoleColor? BackgroundColor { get; set; }
         public ConsoleColor? ForegroundColor { get; set; }
         public bool IsError { get; set; }
         public bool LogFileOnly { get; set; }
-        public string Message { get; set; }
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                // set fallback for any non printable characters
+                _message = Encoding.UTF8.GetString(
+                    Encoding.Convert(Encoding.UTF8, Encoding.GetEncoding(
+                                Encoding.UTF8.EncodingName,
+                                new EncoderReplacementFallback(string.Empty),
+                                new DecoderExceptionFallback()
+                            ),
+                        Encoding.UTF8.GetBytes(value)
+                    )
+                );
+            }
+        }
         public string TimeStamp { get; set; }
     }
 }
