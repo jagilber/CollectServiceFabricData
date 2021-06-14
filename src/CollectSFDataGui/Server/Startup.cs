f3514c6afabbe130defaa348c876e3d9fc0d6830
PurnaChandraPanda <pupanda@outlook.com>
2021-06-14 21:29:14 +0530

    swagger name change

--- src/CollectSFDataGui/Server/Startup.cs
+++ src/CollectSFDataGui/Server/Startup.cs
@@ -26,7 +26,7 @@ namespace CollectSFDataGui.Server
                 app.UseWebAssemblyDebugging();
 
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApplication1 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CollectSFDataGui v1"));
             }
             else
             {
@@ -57,7 +57,7 @@ namespace CollectSFDataGui.Server
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CollectSFDataGui", Version = "v1" });
             });
         }
     }
